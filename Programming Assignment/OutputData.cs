3f05590a8da15a34c8773e40f0af26f356d2beb7
Ben Stavig <bstavig5299@floridapoly.edu>
2021-04-28 13:11:10 -0400

    Replaced Old Files

---
+++ Programming Assignment/OutputData.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Assignment
+{
+    /// <summary>
+    /// This is a class that holds the response data from uploading the file
+    /// </summary>
+    public class UploadData
+    {
+        public string data_id { get; set; }
+        public string status { get; set; }
+        public int in_queue { get; set; }
+        public string queue_priority { get; set; }
+        public string sha1 { get; set; }
+        public string sha256 { get; set; }
+    }
+
+    /// <summary>
+    /// NOT IMPLEMENTED
+    /// This holds response data from the API if they wish to do so, this is not used anywhere and can be removed if desired
+    /// </summary>
+    public class Data
+    {
+        public int scan_result_history_length { get; set; }
+        public string file_id { get; set; }
+        public string data_id { get; set; }
+        public Sanitized sanitized { get; set; }
+        public Process_Info process_info { get; set; }
+        public Scan_Results scan_results { get; set; }
+        public File_Info file_info { get; set; }
+        public int share_file { get; set; }
+        public string rest_version { get; set; }
+        public object[] additional_info { get; set; }
+        public Votes votes { get; set; }
+    }
+
+    public class Sanitized
+    {
+        public string result { get; set; }
+        public int progress_percentage { get; set; }
+        public string data_id { get; set; }
+        public string reason { get; set; }
+    }
+
+    public class Process_Info
+    {
+        public string result { get; set; }
+        public string profile { get; set; }
+        public Post_Processing post_processing { get; set; }
+        public bool file_type_skipped_scan { get; set; }
+        public string blocked_reason { get; set; }
+    }
+
+    public class Post_Processing
+    {
+        public string copy_move_destination { get; set; }
+        public string converted_to { get; set; }
+        public string converted_destination { get; set; }
+        public string actions_ran { get; set; }
+        public string actions_failed { get; set; }
+    }
+
+    public class Scan_Results
+    {
+        public Scan_Details scan_details { get; set; }
+        public bool rescan_available { get; set; }
+        public int scan_all_result_i { get; set; }
+        public DateTime start_time { get; set; }
+        public int total_time { get; set; }
+        public int total_avs { get; set; }
+        public int total_detected_avs { get; set; }
+        public int progress_percentage { get; set; }
+        public string scan_all_result_a { get; set; }
+    }
+
+    public class Scan_Details
+    {
+        public Zillya Zillya { get; set; }
+        public XvirusPersonalGuard XvirusPersonalGuard { get; set; }
+        public Virusblokada VirusBlokAda { get; set; }
+        public TrendmicroHouseCall TrendMicroHouseCall { get; set; }
+        public Trendmicro TrendMicro { get; set; }
+        public TotalDefense TotalDefense { get; set; }
+        public Threattrack ThreatTrack { get; set; }
+        public TACHYON TACHYON { get; set; }
+        public Sophos Sophos { get; set; }
+        public Superantispyware SUPERAntiSpyware { get; set; }
+        public QuickHeal QuickHeal { get; set; }
+        public Preventon Preventon { get; set; }
+        public NANOAV NANOAV { get; set; }
+        public Mcafee McAfee { get; set; }
+        public K7 K7 { get; set; }
+        public Jiangmin Jiangmin { get; set; }
+        public Ikarus Ikarus { get; set; }
+        public Huorong Huorong { get; set; }
+        public Hauri Hauri { get; set; }
+        public Fortinet Fortinet { get; set; }
+        public Filseclab Filseclab { get; set; }
+        public FSecure Fsecure { get; set; }
+        public FProt Fprot { get; set; }
+        public Emsisoft Emsisoft { get; set; }
+        public ESET ESET { get; set; }
+        public Cyren Cyren { get; set; }
+        public Clamav ClamAV { get; set; }
+        public Bytehero ByteHero { get; set; }
+        public Bitdefender BitDefender { get; set; }
+        public Baidu Baidu { get; set; }
+        public Avira Avira { get; set; }
+        public Antiy Antiy { get; set; }
+        public Ahnlab Ahnlab { get; set; }
+        public Agnitum Agnitum { get; set; }
+        public VirITExplorer VirITeXplorer { get; set; }
+        public VirITML VirITML { get; set; }
+    }
+
+    public class Zillya
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class XvirusPersonalGuard
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Virusblokada
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class TrendmicroHouseCall
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Trendmicro
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class TotalDefense
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Threattrack
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class TACHYON
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Sophos
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Superantispyware
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class QuickHeal
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Preventon
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class NANOAV
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Mcafee
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class K7
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Jiangmin
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Ikarus
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Huorong
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Hauri
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Fortinet
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Filseclab
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class FSecure
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class FProt
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Emsisoft
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class ESET
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Cyren
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Clamav
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Bytehero
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Bitdefender
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Baidu
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Avira
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Antiy
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Ahnlab
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class Agnitum
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class VirITExplorer
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class VirITML
+    {
+        public string threat_found { get; set; }
+        public int scan_time { get; set; }
+        public int scan_result_i { get; set; }
+        public DateTime def_time { get; set; }
+    }
+
+    public class File_Info
+    {
+        public int file_size { get; set; }
+        public DateTime upload_timestamp { get; set; }
+        public string md5 { get; set; }
+        public string sha1 { get; set; }
+        public string sha256 { get; set; }
+        public string file_type_category { get; set; }
+        public string file_type_description { get; set; }
+        public string file_type_extension { get; set; }
+        public string display_name { get; set; }
+    }
+
+    public class Votes
+    {
+        public int up { get; set; }
+        public int down { get; set; }
+    }
+
+}
---
+++ Programming Assignment/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace Programming_Assignment
+{
+
+
+    class Program
+    {
+        //Place API key here
+        public static readonly string ApiKey = "INSERT KEY HERE";
+
+        
+        static void Main(string[] args)
+        {
+            //Not nessasary to type "upload_file", can be anything, but is required to seperate the user input with a space.
+            Console.WriteLine("Type \"upload_file\" and the file you would like to upload.");
+            string Command = Console.ReadLine();
+            string[] arguments = Command.Split(' ');
+
+            //Initizlize the file control that handles getting its hash value
+            FileControl file = new FileControl(arguments[1]);
+
+            //runs the first task that looks up the file by Hash
+            var t1 = Task.Run(() => HashLookUpAsync(file));
+
+            //if it doesnt find it, then this enters and attempts to upload the fille
+            if (!t1.Result)
+            {
+                Console.WriteLine("\nAttempting to Upload and Scan File\n");
+                var t2 = Task.Run(() => FileUploadAsync(file));
+                //enters if the file is uploaded and retrieves the data with a call by its ID
+                if (t2.Result)
+                {
+                    Console.WriteLine("Printing Scan Details");
+                    var t3 = Task.Run(() => FileScanDataIdAsync(file));
+                }
+            }
+
+            //prevents the console from closing and waits for tasks to finish
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the Hash from the file, and attempts to look it up, returns false if the file can not be found
+        /// or true and prints out the previous scan results
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static async Task<bool> HashLookUpAsync(FileControl file)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.metadefender.com/v4/hash/" + file.Hash)) //Starts the Get Command
+                {
+                    request.Headers.TryAddWithoutValidation("apikey", ApiKey); //adds the api key as a header for access
+
+                    var response = await httpClient.SendAsync(request); //Sends command
+                    string responseString = await response.Content.ReadAsStringAsync(); //retrieves response from API
+                    dynamic data1 = JsonConvert.DeserializeObject(responseString); // gets a dynamic data type of the string that is returned
+                    
+                    if (!responseString.Contains("error")) //Simply checks if the message is either an error or not, not the best use, can be improved
+                    {
+                        Console.WriteLine("\nAlready Chached File\n");
+                        foreach (object d in data1)
+                        {
+                            Console.WriteLine(d); //prints every line of the dynamic data
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        return false; //returns false if there is NOT a file already in the system
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Attempts to upload the file and gets the data_ID to pull the scan results
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static async Task<bool> FileUploadAsync(FileControl file)
+        {
+            using var httpClient = new HttpClient();
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api.metadefender.com/v4/file")) //Post command, similar to the above statement
+            {
+                request.Headers.TryAddWithoutValidation("apikey", ApiKey);
+                request.Headers.TryAddWithoutValidation("sandbox", "windows10");
+
+                request.Content = new StringContent(Regex.Replace(File.ReadAllText(file.DocPath + "\\" + file.FileName), "(?:\\r\\n|\\n|\\r)", string.Empty));
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
+
+                var response = await httpClient.SendAsync(request);
+
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                UploadData data = System.Text.Json.JsonSerializer.Deserialize<UploadData>(responseString); //This is wasted space, but does initilize everything in the Output data file
+                //Could be implemented to neatly print out everything
+                
+                if (!responseString.Contains("error"))
+                {
+                    file.Data_Id = data.data_id;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("\nERROR, failed to upload File\n");
+                    return false;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Pulls Scan results by data ID, very similar to the HashLookUp method above
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static async Task<bool> FileScanDataIdAsync(FileControl file)
+        {
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.metadefender.com/v4/file/" + file.Data_Id);
+            request.Headers.TryAddWithoutValidation("apikey", ApiKey);
+            var response = await httpClient.SendAsync(request);
+            string responseString = await response.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(responseString);
+            
+            if (!responseString.Contains("error"))
+            {
+                foreach (object d in data)
+                {
+                    Console.WriteLine(d);
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+                return false;
+            }
+        }
+    }
+   
+
+
+
+
+    /// <summary>
+    /// Holds relevents data based off the file the user entered. Is required for the file to be in documents
+    /// Or the user can modify the code to look for something more specific without moving the data around
+    /// </summary>
+    class FileControl
+    {
+        public string FileName;
+        public string DocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public string AnyPath = "C:\\Users\\stavi\\Desktop";
+        public string Hash;
+        public string Data_Id;
+
+        //Constuctor that calls the hashing method
+        public FileControl(string FileName)
+        {
+            this.FileName = FileName;
+            Hash = GetHash(this.FileName);
+        }
+
+        //Simple Hashing
+        private string GetHash(string FileName)
+        {
+            using MD5 md5 = MD5.Create();
+            using Stream stream = File.OpenRead(DocPath + "\\" + FileName);
+            byte[] hashBit = md5.ComputeHash(stream);
+            return BitConverter.ToString(hashBit).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
